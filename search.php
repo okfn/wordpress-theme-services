07fe91ce8ea548a9483a9d100cf56b18cc17fff5
Sam Smith <sam@mintcanary.com>
2014-06-26 17:38:14 +0100

    search layout

--- search.php
+++ search.php
@@ -1,10 +1,20 @@
 <?php get_header(); ?>
+
+<header class="banner" role="banner">
+  <div class="row">
+		<div class="medium-6 columns end">
+      <h1 class="entry-title"><?php _e('Search Results for', 'FoundationPress'); ?> "<?php echo get_search_query(); ?>"</h1>
+      <div class="intro">
+        &nbsp;
+      </div>
+		</div>
+	</div>
+</header>
 <div class="row">
-	<div class="small-12 large-8 columns" role="main">
+	<div class="small-12 columns" role="main">
 	
 		<?php do_action('foundationPress_before_content'); ?>
 	
-		<h2><?php _e('Search Results for', 'FoundationPress'); ?> "<?php echo get_search_query(); ?>"</h2>
 	
 	<?php if ( have_posts() ) : ?>
 	
@@ -30,6 +40,5 @@
 	<?php do_action('foundationPress_after_content'); ?>
 
 	</div>
-	<?php get_sidebar(); ?>
 		
 <?php get_footer(); ?>
