1e1dd0ead15d27fdf08f771a1494b683052f0f42
Sam Smith <sam@mintcanary.com>
2014-09-11 18:29:17 +0100

    banner colour option

---
+++ library/banner.php
@@ -0,0 +1,16 @@
+<?
+$thumb_id = get_post_thumbnail_id();
+$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
+$thumb_url = $thumb_url_array[0];
+?>
+
+<header class="banner" role="banner" style=" <? if(get_field('banner_colour')) { echo 'background-color:' . get_field('banner_colour') . ';';}  if ( has_post_thumbnail() ) {	?> background-image:url(<? echo $thumb_url; ?>) <? ;} ?> ">
+  <div class="row">
+		<div class="small-12<? if ( has_post_thumbnail() ) {	?> medium-6<? ;} ?> columns end">
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <div class="intro">
+        <?php the_field('intro'); ?>
+      </div>
+		</div>
+	</div>
+</header>
--- page.php
+++ page.php
@@ -4,22 +4,7 @@
 	
 <?php while (have_posts()) : the_post(); ?>
 
-<?
-$thumb_id = get_post_thumbnail_id();
-$thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
-$thumb_url = $thumb_url_array[0];
-?>
-
-<header class="banner" role="banner" <? if ( has_post_thumbnail() ) {	?> style="background-image:url(<? echo $thumb_url; ?>)" <? ;} ?> >
-  <div class="row">
-		<div class="small-12<? if ( has_post_thumbnail() ) {	?> medium-6<? ;} ?> columns end">
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <div class="intro">
-        <?php the_field('intro'); ?>
-      </div>
-		</div>
-	</div>
-</header>
+<? include ("library/banner.php"); ?>
 <div class="row">
 	<div class="small-12 columns" role="main">
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
